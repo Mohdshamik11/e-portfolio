0f2094300b01100ccfefda898024d91d78531fdf
Mohdshamik11 <mdshamik@outlook.com>
2026-09-09 20:08:15 +0800

    adjust headshot

--- components/site/Hero.tsx
+++ components/site/Hero.tsx
@@ -93,7 +93,7 @@ export function Hero() {
                 width: "100%",
                 height: "100%",
                 objectFit: "cover",
-                objectPosition: "center 12%",
+                objectPosition: "center 18%",
               }}
             />
           ) : (
